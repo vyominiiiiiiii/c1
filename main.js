1d20ff27260102ebc325c46522571fd2a4bc6c4d
vyominiiiiiiii <102034432+vyominiiiiiiii@users.noreply.github.com>
2024-11-30 10:59:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var name="vyomini"; //this is a string
+var number="1.7";
+
+console.log(name,number);
+
+var name2=true;
+console.log(name2);
+
+console.log(typeof name);
+console.log(typeof number);
+console.log(typeof name2);
+
+var name3;  /*this has a value undefined */
+console.log(name3);
+
+var name4=8;
+console.log(number+name4);
+console.log(number-name4);
+console.log(number*name4);
+console.log(number/name4);
+
+var number2=3;
+console.log(name4%number2);
